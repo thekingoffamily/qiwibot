ecb82f73e68e0a74bbe32c54885450452a257f40
Vitaliy <thekingoffamily2017@gmail.com>
2019-02-19 18:29:53 +0300

    +code

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import pyqiwi
 token = '764914862:AAGfQycEKhlApnEKOsGBlkmytNbk9NzD0bE'
 bot = telebot.TeleBot(token)
 
+wallet = pyqiwi.Wallet(token='d25424a423bdb129db09b4c5f4ddee66', number='79164763578')
+balance = wallet.balance()
 
 
 
 @bot.message_handler(commands=['start'])
 def mes_on_start(message):
-    wallet = pyqiwi.Wallet(token='d25424a423bdb129db09b4c5f4ddee66', number='79164763578')
-    balance = wallet.balance()
-    bot.send_message(message.from_user.id, balance)
+    bot.send_message(message.from_user.id, text=balance)
 
 
 
