4d1e65eaf1ae95ab0ccbcff7b9cafaed35c6330e
Vitaliy <thekingoffamily2017@gmail.com>
2019-02-20 18:02:28 +0300

    +code

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import telebot
 import pyqiwi
-
+from telebot import apihelper
 
 token = '764914862:AAGfQycEKhlApnEKOsGBlkmytNbk9NzD0bE'
 bot = telebot.TeleBot(token)
 
 
+def balance():
+    apihelper.proxy = {'http':'http://178.206.224.176:3128'}
+    wallet = pyqiwi.Wallet('d25424a423bdb129db09b4c5f4ddee66', '79164763578')
+    balance = wallet.balance()
 
 
 
 @bot.message_handler(commands=['start'])
 def mes_on_start(message):
-    wallet = pyqiwi.Wallet('d25424a423bdb129db09b4c5f4ddee66', '79164763578')
-    balance = wallet.balance()
-    bot.send_message(message.from_user.id, text=("Баланс 89164763578 сост. = ", balance))
+    bot.send_message(message.from_user.id, text=str(balance()))
 
 
 
