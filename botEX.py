3ac1c63529ed51b3c46d82875c375157bce41da9
Vitaliy <thekingoffamily2017@gmail.com>
2019-02-21 11:31:44 +0300

    +code

--- botEX.py
+++ botEX.py
@@ -1,10 +1,31 @@
 import telebot
-from telebot import apihelper
+import requests
+import json
 
-apihelper.proxy = {'http':'74.208.83.188:80'}
+proxies = {
+            'http' : "socks4://145.255.6.171:53177",
+            'https' : "socks4://145.255.6.171:53177"
+        }
+
+api_access_token = 'be643fe09cf46af7297f94b25ecc1ace'
+s = requests.Session()
+s.headers['authorization'] = 'Bearer ' + api_access_token
+b = s.get('https://edge.qiwi.com/funding-sources/v1/accounts/current', proxies=proxies)
+info = json.loads(b.text)
+
+def get_balance():
+    i = []
+    for i in info['accounts']:
+        return i['balance']
+    print(i)
+
+get = get_balance()
+
+balanc = get.get('amount')
+
 
 token = '764914862:AAGfQycEKhlApnEKOsGBlkmytNbk9NzD0bE'
 
 bot = telebot.TeleBot(token)
 
-bot.send_message(361395559, "Hello")
+bot.send_message(361395559, str(balanc))
